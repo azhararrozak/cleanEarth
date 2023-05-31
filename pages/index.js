71ce2490fc9b041246e5ff0d47ec99491aecedc9
Sarah Larasati <108144344+sarahlarasati@users.noreply.github.com>
2023-05-31 12:43:27 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,62 @@
+
 const Home = () => {
   return (
-    <div>HOME</div>
+    <div class="home">
+
+      <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+          <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="/img/Trash11.jpg" class="d-block w-100" alt="..." />
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Welcome To Cleanearth</h5>
+              <button type="button" class="btn btn-light">More</button>
+              <p>Keep the earth clean for our future generations.</p>
+            </div>
+          </div>
+
+          <div class="carousel-item">
+            <img src="/img/Trash22.jpg" class="d-block w-100" alt="..." />
+            <div class="carousel-caption d-none d-md-block">
+              <h5>let's process waste into useful items</h5>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+          </div>
+
+          <div class="carousel-item">
+            <img src="/img/Trash33.jpg" class="d-block w-100" alt="..." />
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Dispose of your trash properly</h5>
+              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            </div>
+          </div>
+
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+
+      <section class="section">
+        <div class="container">
+
+          <div class="section_header">
+            <h2 class="title section_title">Mari Jaga Bumi Kita</h2></div>
+          
+        </div>
+      </section>
+
+
+    </div>
   )
 }
 
