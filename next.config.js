d3d52cb0b0689dbf7407db79880d66ea60cdcd5d
azhararrozak <azhardrozak2001@upi.edu>
2023-06-17 16:24:41 +0700

    Ubah Base URL

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 // DB-Local: mongodb://localhost:27017/DBForWebsite
 module.exports = {
   env: {
-    "BASE_URL": "http://localhost:3000",
+    "BASE_URL": "https://clean-earth.vercel.app/",
     "MONGODB_URL": "mongodb+srv://DBForWebsite:DBForWebsite@dbforwebsite.5f45os9.mongodb.net/?retryWrites=true&w=majority",
     "ACCESS_TOKEN_SECRET": "}'L*/3HT!aAR*a:9~B~X{fYa`%)8N8g>*vGkqq7d7}KG9S;}",
     "REFRESH_TOKEN_SECRET": "gLut&Hr,5]}m,*Ym'Ft(Q{#L7mq;jfTuFX!3p:GWV8_J#(`>>>Md*$~{j!P)E~R&/,;VwaM)RQGw_(",
