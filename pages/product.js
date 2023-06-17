db701190e356139d9775ecad17dc59bb47b283ca
D12oZ <31308758+azhararrozak@users.noreply.github.com>
2023-06-17 14:27:32 +0700

    Update product.js

--- pages/product.js
+++ pages/product.js
@@ -1,27 +1,43 @@
-/* eslint-disable react/no-unescaped-entities */
-/* eslint-disable @next/next/no-img-element */
-import Head from "next/link";
-import ProductItem from "@/components/ProductItem";
+import { getData } from "@/utils/dataFetch";
+import React, { useState } from "react";
+import Head from "next/head";
+import ProductItem from "@/components/ProductItem"
 
-const Home = () => {
+const Product = (props) => {
+  const [products, setProducts] = useState(props.products);
+
+
   return (
     <div className="products">
       <Head>
         <title>Product Page</title>
       </Head>
-      
+
       {
         products.length === 0 
         ? 
-          <h2>Nothing Product List</h2> 
+          <h2>Nothing Product list</h2> 
         :
           products.map(product => (
             <ProductItem key={product._id} product={product} />
           ))
       }
-    </div>
-
+       </div>
   );
 };
 
-export default Home;
+// server side rendering
+export async function getServerSideProps() {
+
+  const res = await getData("product");
+  console.log(res);
+
+  return {
+    props: {
+      products: res.products,
+      result: res.result,
+    },
+  };
+}
+
+export default Product;
